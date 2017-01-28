e2b426c396c9a0745738d20dce36ad91b8af7b44
Alex Johnston <alex.wade.johnston@gmail.com>
2017-01-28 17:41:49 -0600

    Made code more expandable, improved font

--- _8x8-LED-Matrix/_8x8-LED-Matrix.ino
+++ _8x8-LED-Matrix/_8x8-LED-Matrix.ino
@@ -1,47 +1,19 @@
 
 //ST_CP
 int latchPin = 8;
-
 //SH_CP
 int clockPin = 12;
-
 //DS
 int dataPin = 11;
+const int numberOfModules = 1;
 
-// This is for an 8x7seg display
-//byte nums[11] = {111,6,91,31,54,61,125,7,127,55,0};
+// Font Info
+byte letterWidth = 6;
+byte letters[127][8] =  {{0,0,0,0,0,0,0,0},{0,4,4,4,4,4,0,4},{0,10,10,10,0,0,0,0},{0,0,10,31,10,31,10,0},{0,4,30,1,14,16,15,4},{0,2,21,10,4,10,21,8},{0,6,9,9,6,21,9,22},{0,4,4,4,0,0,0,0},{0,16,8,8,8,8,8,16},{0,4,8,8,8,8,8,4},{0,0,8,28,8,28,8,0},{0,0,8,8,62,8,8,0},{0,0,0,0,0,8,8,4},{0,0,0,0,62,0,0,0},{0,0,0,0,0,0,12,12},{0,16,8,12,4,6,2,1},{0,14,25,29,21,23,19,14},{0,4,6,4,4,4,4,14},{0,14,17,16,8,4,2,31},{0,14,17,16,14,16,17,14},{0,8,12,10,31,8,8,8},{0,31,1,1,15,16,17,14},{0,14,17,1,15,17,17,14},{0,31,16,8,4,4,2,2},{0,14,17,17,14,17,17,14},{0,14,17,17,30,16,17,14},{0,0,6,6,0,6,6,0},{0,0,6,6,0,4,6,2},{0,8,4,2,1,2,4,8},{0,0,0,31,0,31,0,0},{0,4,8,16,32,16,8,4},{0,14,17,16,12,4,0,4},{0,14,17,21,21,29,1,30},{0,14,17,17,31,17,17,17},{0,15,17,17,15,17,17,15},{0,14,17,1,1,1,17,14},{0,15,17,17,17,17,17,15},{0,31,1,1,15,1,1,31},{0,31,1,1,15,1,1,1},{0,14,17,1,25,17,17,14},{0,17,17,17,31,17,17,17},{0,31,4,4,4,4,4,31},{0,31,4,4,4,4,5,2},{0,17,9,5,3,5,9,17},{0,4,4,4,4,4,4,8},{0,17,27,21,17,17,17,17},{0,17,19,19,21,25,25,17},{0,14,17,17,17,17,17,14},{0,15,17,17,15,1,1,1},{0,14,17,17,17,21,25,30},{0,15,17,17,15,5,9,17},{0,14,17,1,14,16,17,14},{0,31,4,4,4,4,4,4},{0,17,17,17,17,17,17,14},{0,17,17,17,17,10,10,4},{0,17,17,17,17,21,27,17},{0,17,17,10,4,10,17,17},{0,17,17,10,4,4,4,4},{0,31,16,8,4,2,1,31},{0,14,2,2,2,2,2,14},{0,1,2,6,4,12,8,16},{0,14,8,8,8,8,8,12},{0,4,10,17,0,0,0,0},{0,0,0,0,0,0,0,31},{0,1,2,4,0,0,0,0},{0,0,0,14,16,30,17,30},{0,1,1,1,15,17,17,15},{0,0,0,14,17,1,17,14},{0,16,16,16,30,17,17,30},{0,0,0,14,17,31,1,14},{0,8,20,4,14,4,4,4},{0,0,0,30,17,30,16,14},{0,1,1,1,15,17,17,17},{0,0,0,4,0,4,4,4},{0,0,0,4,0,4,4,2},{0,1,1,9,5,3,5,9},{0,4,4,4,4,4,4,8},{0,0,0,11,21,21,21,21},{0,0,0,14,18,18,18,18},{0,0,0,14,17,17,17,14},{0,0,0,15,17,15,1,1},{0,0,0,30,17,30,16,16},{0,0,0,15,17,1,1,1},{0,0,0,30,1,14,16,15},{0,4,4,14,4,4,4,8},{0,0,0,17,17,17,17,30},{0,0,0,17,17,17,10,4},{0,0,0,17,21,21,21,10},{0,0,0,17,10,4,10,17},{0,0,0,17,17,30,16,14},{0,0,0,31,8,4,2,31},{0,24,4,4,2,4,4,24},{0,4,4,4,4,4,4,4},{0,6,8,8,16,8,8,6},{0,0,0,9,21,18,0,0}};
+char text[] = "0123456789";
 
-
-byte letters[26][8] =  {{24,36,66,66,126,66,66,66},
-                        {62,66,66,62,66,66,66,62},
-                        {60,66,2,2,2,2,66,60},
-                        {30,34,66,66,66,66,34,30},
-                        {126,2,2,62,2,2,2,126},
-                        {126,2,2,62,2,2,2,2},
-                        {60,66,2,2,114,66,66,60},
-                        {66,66,66,126,66,66,66,66},
-                        {62,8,8,8,8,8,8,62},
-                        {124,16,16,16,16,16,18,12},
-                        {34,18,10,6,6,10,18,34},
-                        {2,2,2,2,2,2,2,126},
-                        {66,102,90,66,66,66,66,66},
-                        {66,70,74,74,82,82,98,66},
-                        {60,66,66,66,66,66,66,60},
-                        {62,66,66,66,62,2,2,2},
-                        {60,66,66,66,82,98,66,188},
-                        {62,66,66,66,62,18,34,66},
-                        {60,66,2,2,60,64,66,60},
-                        {62,8,8,8,8,8,8,8},
-                        {66,66,66,66,66,66,66,60},
-                        {66,66,66,66,36,36,24,24},
-                        {66,66,66,66,66,90,102,66},
-                        {66,66,36,24,24,36,66,66},
-                        {66,66,66,124,64,64,64,60},
-                        {126,64,32,16,8,4,2,126}};
-
-
-char text[] = "HELLO WORLD";
-unsigned short disp[8] = {0,0,0,0,0,0,0,0};
+int cycleSpeed = 256;
+byte disp[numberOfModules][8];
 
 void setup() {
   //set pins to output so you can control the shift register
@@ -49,45 +21,65 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  
 }
 
 void loop() {
-  for (int i = 0; i < 8; i++){
-    disp[i] &= 0x00FF;
+  
+  
+  clearDisplay();
+  for (int i = 0; i < sizeof(text) - 1; i++){
+    cycleDisplayLeftByCharacter(text[i], cycleSpeed);
   }
-  for (int i = 0; i < sizeof(text); i++){
-    for (int j = 0; j < 8; j++){
-      for (int k = 0; k < 128; k++){
-        writeToDisplay( 1 << (k % 8), (byte)(disp[k % 8] ) );
-      }
-      for (int k = 0; k < 8; k++){
-        disp[k] = disp[k] >> 1;
-      }
-    }
-    for (int j = 0; j < 8; j++){
-      if (32 != text[i]){
-        disp[j] |= (letters[text[i] - 65][j] << 8);
-      }else{
-        disp[j] &= 0x00FF;
-      }
-    }
+  for (int i = 0; i < (numberOfModules * 8) - letterWidth; i++){
+    cycleDisplayLeftByCharacter(' ', cycleSpeed);
   }
- 
 }
 
-void writeToDisplay(byte row, byte column){
-      digitalWrite(latchPin, LOW);
 
-      // The two 8bit registers are in series. First is the Colum, then the Row.
-      //The Row needs to be written to first because it needs to be pushed through the Column register
-      shiftOut(dataPin, clockPin, MSBFIRST, row);
-      shiftOut(dataPin, clockPin, MSBFIRST, column);
-
-      digitalWrite(latchPin, HIGH);
+void cycleDisplayLeftByCharacter(byte character, int dispSpeed){
+    for (int m = 0; m < letterWidth; m++){
+      for (int j = 0; j < 8; j++){
+        byte carry = ( getLetterPart(character,j) << ( 7 - m )) & 0x80;
+        cycleDisplayRowLeft( j, carry );
+      }
+      writeToDisplay(dispSpeed);
+    }
 }
 
 
 
-    
+void cycleDisplayRowLeft(byte row, byte carry){
+    for (int k = numberOfModules - 1; k >= 0; k--){
+      byte tempCarry = (disp[k][row] & 0x80) << 7;
+      disp[k][row] = disp[k][row] >> 1;
+      disp[k][row] |= carry;
+      carry = tempCarry;
+    } 
+}
+
+byte getLetterPart(byte letter, byte row){
+
+  return letters[letter - 32][row];
+}
+
+void clearDisplay(){
+  for (int i = 0; i < numberOfModules; i++){
+    for (int j = 0; j < 8; j++){
+      disp[i][j] = 0;
+    }
+  }
+}
+
+void writeToDisplay(int dispSpeed){
+      for (int i = 0; i < dispSpeed; i++){
+        digitalWrite(latchPin, LOW);
+        shiftOut(dataPin, clockPin, MSBFIRST, 1 << (i % 8));
+        for (int j = 0; j < numberOfModules; j++){
+          shiftOut(dataPin, clockPin, MSBFIRST, disp[j][i % 8]);
+        }
+        digitalWrite(latchPin, HIGH);
+      }
+}
 
 
